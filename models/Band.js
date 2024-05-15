7a69210db4889f1062be26b7bcea0c141cb69de6
littlegaz <gareth.t.horne@hotmail.co.uk>
2024-05-15 15:19:50 +0100

    band done?

--- models/Band.js
+++ models/Band.js
@@ -1,8 +1,19 @@
+const { STRING } = require('sequelize');
 const {Sequelize, sequelize} = require('../db');
+const { DataTypes } = require('sequelize');
 
 // TODO - define the Band model
-let Band;
+let Band = db.define("Band", {
+    name: DataTypes.STRING,
+    genre: DataTypes.STRING
+});
 
 module.exports = {
     Band
 };
+
+
+
+// In /models/Band.js, define a Band model. The Band model should have the following properties:
+// name: a string
+// genre: a string
